1f85dc4ecee568019b66e056b1ab0dc466e3007b
Jean C. Solís <jcsolisu2000@gmail.com>
2020-08-09 14:04:23 -0600

    Creacion de modulo de ruteo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { PersonaComponent } from './persona/persona.component';
 import { FormularioComponent } from './formulario/formulario.component';
 import { LoggingService } from './LoggingService.service';
 import { PersonasService } from './personas.service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { PersonasService } from './personas.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [LoggingService, PersonasService],
   bootstrap: [AppComponent]
