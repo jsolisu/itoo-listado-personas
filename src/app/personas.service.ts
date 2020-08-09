f476ed1fe01e79cd6025e0ebd237a7c4278e7a3b
Jean C. Solís <jcsolisu2000@gmail.com>
2020-08-09 16:26:38 -0600

    Modificar persona

--- src/app/personas.service.ts
+++ src/app/personas.service.ts
@@ -3,18 +3,27 @@ import { LoggingService } from './LoggingService.service';
 import { Injectable, EventEmitter } from '@angular/core';
 
 @Injectable()
-export class PersonasService{
+export class PersonasService {
     personas: Persona[] = [
-        new Persona("Juan","Perez"), 
-        new Persona("Laura","Juarez")
+        new Persona("Juan", "Perez"),
+        new Persona("Laura", "Juarez")
     ];
 
     saludar = new EventEmitter<number>();
 
-    constructor(private loggingService: LoggingService){}
+    constructor(private loggingService: LoggingService) { }
 
-    agregarPersona(persona: Persona){
+    agregarPersona(persona: Persona) {
         this.loggingService.enviaMensajeAConsola("agregamos persona:" + persona.nombre);
         this.personas.push(persona);
+    }
+
+    encontrarPersona(index: number): Persona {
+        const persona: Persona = this.personas[index];
+        return persona;
+    }
+
+    modificarPersona(index: number, persona: Persona): void {
+        this.personas[index] = persona;
     }
 }
--- src/app/personas/formulario/formulario.component.ts
+++ src/app/personas/formulario/formulario.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Persona } from '../../persona.model';
 import { LoggingService } from '../../LoggingService.service';
 import { PersonasService } from '../../personas.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-formulario',
@@ -13,21 +13,33 @@ export class FormularioComponent implements OnInit {
 
   nombreInput: string;
   apellidoInput: string;
+  index: number;
 
   constructor(private loggingService: LoggingService,
     private personasService: PersonasService,
-    private router: Router) {
+    private router: Router,
+    private route: ActivatedRoute) {
     this.personasService.saludar.subscribe(
-      (indice: number) => alert("El indice es: " + indice)
+      (indice: number) => alert('El indice es: ' + indice)
     );
   }
 
   ngOnInit() {
+    this.index = this.route.snapshot.params['id'];
+    if (this.index) {
+      const persona: Persona = this.personasService.encontrarPersona(this.index);
+      this.nombreInput = persona.nombre;
+      this.apellidoInput = persona.apellido;
+    }
   }
 
   onGuardarPersona() {
     const persona1 = new Persona(this.nombreInput, this.apellidoInput);
-    this.personasService.agregarPersona(persona1);
+    if (this.index) {
+      this.personasService.modificarPersona(this.index, persona1);
+    } else {
+      this.personasService.agregarPersona(persona1);
+    }
 
     this.router.navigate(['personas']);
   }
