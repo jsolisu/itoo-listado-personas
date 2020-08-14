1c8cdfe16340530de93cb36ae5c6b941d1a36289
Jean C. Solís <jcsolisu2000@gmail.com>
2020-08-13 22:11:58 -0600

    Login Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   titulo = 'Listado de Personas';
+
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDP8huv37LpOKnQvTgyRRPtPhhv5oYOm6s',
+      authDomain: 'listado-personas-1ee1c.firebaseapp.com'
+    });
+  }
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,28 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Persona } from './persona.model';
+import { LoginService } from './login/login.service';
 
 @Injectable()
 export class DataService {
-    constructor(private httpClient: HttpClient) { }
+    constructor(private httpClient: HttpClient, private loginService: LoginService) { }
 
     guardarPersonas(personas: Persona[]) {
-        this.httpClient.put('https://listado-personas-1ee1c.firebaseio.com/personas.json', personas).subscribe(
-            response => {
-                console.log(`guardarPersonas: resultado = ${JSON.stringify(response)}`);
-            },
-            error => {
-                console.log(`guardarPersonas: error = ${JSON.stringify(error)}`);
-            }
-        );
+        this.httpClient.put('https://listado-personas-1ee1c.firebaseio.com/personas.json?auth=' + this.loginService.getIdToken(),
+            personas).subscribe(
+                response => {
+                    console.log(`guardarPersonas: resultado = ${JSON.stringify(response)}`);
+                },
+                error => {
+                    console.log(`guardarPersonas: error = ${JSON.stringify(error)}`);
+                }
+            );
     }
 
     cargarPersonas() {
-        return this.httpClient.get('https://listado-personas-1ee1c.firebaseio.com/personas.json');
+        return this.httpClient.get('https://listado-personas-1ee1c.firebaseio.com/personas.json?auth=' + this.loginService.getIdToken());
     }
 
     modificarPersona(index: number, persona: Persona) {
-        const url = 'https://listado-personas-1ee1c.firebaseio.com/personas/' + index + '.json';
+        const url = 'https://listado-personas-1ee1c.firebaseio.com/personas/' + index + '.json?auth=' + this.loginService.getIdToken();
         this.httpClient.put(url, persona).subscribe(
             response => {
                 console.log(`modificarPersona: resultado = ${JSON.stringify(response)}`);
@@ -34,7 +36,7 @@ export class DataService {
     }
 
     eliminarPersona(index: number) {
-        const url = 'https://listado-personas-1ee1c.firebaseio.com/personas/' + index + '.json';
+        const url = 'https://listado-personas-1ee1c.firebaseio.com/personas/' + index + '.json?auth=' + this.loginService.getIdToken();
         this.httpClient.delete(url).subscribe(
             response => {
                 console.log(`eliminarPersona: resultado = ${JSON.stringify(response)}`);
