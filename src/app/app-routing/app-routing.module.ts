1c927ba60eb189f332d45499f0cfc29563712174
Jean C. Solís <jcsolisu2000@gmail.com>
2020-08-14 22:38:42 -0600

    Guardian

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,15 +4,17 @@ import { PersonasComponent } from '../personas/personas.component';
 import { FormularioComponent } from '../personas/formulario/formulario.component';
 import { ErrorComponent } from '../error/error.component';
 import { LoginComponent } from '../login/login.component';
+import { LoginGuardian } from '../login/login-guardian.service';
 
 const routes: Routes = [
-  { path: '', component: PersonasComponent },
+  { path: '', component: PersonasComponent, canActivate: [LoginGuardian] },
   {
-    path: 'personas', component: PersonasComponent, children: [{ path: 'agregar', component: FormularioComponent },
+    path: 'personas', component: PersonasComponent, canActivate: [LoginGuardian],
+    children: [{ path: 'agregar', component: FormularioComponent },
     { path: ':id', component: FormularioComponent }]
   },
-  { path: 'login', component: LoginComponent},
-  { path: '**', component: ErrorComponent}
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: ErrorComponent }
 ];
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,3 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { PersonaComponent } from './personas/persona/persona.component';
 import { FormularioComponent } from './personas/formulario/formulario.component';
@@ -11,9 +7,13 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { PersonasComponent } from './personas/personas.component';
 import { ErrorComponent } from './error/error.component';
 import { DataService } from './data.service';
-import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { LoginService } from './login/login.service';
+import { LoginGuardian } from './login/login-guardian.service';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -30,7 +30,7 @@ import { LoginService } from './login/login.service';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [LoggingService, PersonasService, DataService, LoginService],
+  providers: [LoggingService, PersonasService, DataService, LoginService, LoginGuardian],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
